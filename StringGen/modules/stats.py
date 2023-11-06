3741804859462b7146ea993d9f622f1d3e3ef627
Meyit47 <148498525+Meyit47@users.noreply.github.com>
2023-11-06 13:47:04 +0300

    Update stats.py

--- StringGen/modules/stats.py
+++ StringGen/modules/stats.py
@@ -9,4 +9,4 @@ from StringGen.utils import get_served_users
 @Alone.on_message(filters.command(["stats", "users"]) & filters.user(OWNER_ID))
 async def get_stats(_, message: Message):
     users = len(await get_served_users())
-    await message.reply_text(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ {Alone.name} :\n\n {users} ᴜsᴇʀs")
+    await message.reply_text(f"» Şu andaki istatistikler {Alone.name} :\n\n {users} KULLANICILAR")
