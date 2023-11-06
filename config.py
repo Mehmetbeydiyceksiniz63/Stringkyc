c8e93eddc39ad70a71e5fbb212f94db84d24a042
Meyit47 <148498525+Meyit47@users.noreply.github.com>
2023-11-06 17:56:40 +0300

    Update config.py
    Signed-off-by: Meyit47 <148498525+Meyit47@users.noreply.github.com>

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", 2930013))
 API_HASH = getenv("API_HASH", "7cab92dcd979add511b79d693775e17d")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6680893861:AAGBG1gB-Q6faC_-mAf7EcFq_k3n_CXPE9w")
+BOT_TOKEN = getenv("BOT_TOKEN", "6788830543:AAFtjCy0w9zjbi3CknH-zVBWik4mvwL9w4E")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://meyit31:Myt476331@cluster0.psvz6dk.mongodb.net/?retryWrites=true&w=majority")
 
 OWNER_ID = int(getenv("OWNER_ID", 1527476177))
