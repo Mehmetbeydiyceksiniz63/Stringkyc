5cfa648ae38c47b13e3bc16a2cc4c8e3cba12993
Rq7mg <163336518+Rq7mg@users.noreply.github.com>
2024-06-20 06:24:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", 2930013))
-API_HASH = getenv("API_HASH", "7cab92dcd979add511b79d693775e17d")
+API_ID = int(getenv("API_ID", 28920866))
+API_HASH = getenv("API_HASH", "833ed2af149085968fff77d2fd843f51")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6788830543:AAFtjCy0w9zjbi3CknH-zVBWik4mvwL9w4E")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://meyit31:Myt476331@cluster0.psvz6dk.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "7297331035:AAEyslXtWrvwEPdsWgGM382lUY8hZYszi-w")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ihsantopic:ihsanbot@cluster0.jfejxle.mongodb.net/?retryWrites=true&w=majority")
 
-OWNER_ID = int(getenv("OWNER_ID", 1527476177))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/S1F1RB1RSOHBET")
+OWNER_ID = int(getenv("OWNER_ID", 6563936773))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/kiyicitayfaa")
