23d7173c4d02e8257f67c89e5385b28594e2873c
Rq7mg <163336518+Rq7mg@users.noreply.github.com>
2024-08-04 00:56:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", 28920866))
-API_HASH = getenv("API_HASH", "833ed2af149085968fff77d2fd843f51")
+API_ID = int(getenv("API_ID", 22659994))
+API_HASH = getenv("API_HASH", "2c89964a0088a7a39ec819c60ae67de7")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "7297331035:AAEyslXtWrvwEPdsWgGM382lUY8hZYszi-w")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ihsantopic:ihsanbot@cluster0.jfejxle.mongodb.net/?retryWrites=true&w=majority")
