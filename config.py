a24bffe4bd211e1535d5138fd245137530475707
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2024-08-13 21:30:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", 22659994))
+API_ID = int(getenv("API_ID", "22659994"))
 API_HASH = getenv("API_HASH", "2c89964a0088a7a39ec819c60ae67de7")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7297331035:AAEyslXtWrvwEPdsWgGM382lUY8hZYszi-w")
+BOT_TOKEN = getenv("BOT_TOKEN", "7439639831:AAGVCRa7TNt5Gh5vhw2FRNeFubTHeA-w6qI")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ihsantopic:ihsanbot@cluster0.jfejxle.mongodb.net/?retryWrites=true&w=majority")
 
-OWNER_ID = int(getenv("OWNER_ID", 6563936773))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/kiyicitayfaa")
+OWNER_ID = int(getenv("OWNER_ID", "6604501109"))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/kumsalmuzikk")
