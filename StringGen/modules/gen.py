1209ff16dcb1a8dab2b884cda79a9cba6ac13b8f
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2024-08-13 21:16:18 +0300

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@S1F1RB1RSOHBETT"))
+            await client(JoinChannelRequest("kumsaldestekkanal"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
@@ -239,7 +239,7 @@ async def gen_session(
                 txt.format(ty, string_session, SUPPORT_CHAT),
                 disable_web_page_preview=True,
             )
-            await client.join_chat("S1F1RB1RSOHBET")
+            await client.join_chat("kumsalmuzikk")
     except KeyError:
         pass
     try:
