25c258b4f3564122e4a43ec5f2d6f83272d64f1f
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2024-08-23 19:26:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "22659994"))
 API_HASH = getenv("API_HASH", "2c89964a0088a7a39ec819c60ae67de7")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7037897729:AAHzFNNW6b9X0SNoz2cXUVmiQoHySDHuBF8")
+BOT_TOKEN = getenv("BOT_TOKEN", "7381395088:AAGCrCkgrNfP0AgIG4b5bz6HH6rj694utRY")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://kurt67143:nays@cluster0.vjg7bma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 OWNER_ID = int(getenv("OWNER_ID", "7268753735"))
